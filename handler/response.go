2430ac9b903dbb3caeefa6da099de531b646a591
novdov <rpwjd56@gmail.com>
2020-06-21 09:43:22 +0900

    fea: Add userResponse

--- handler/response.go
+++ handler/response.go
@@ -14,12 +14,31 @@ type profileResponse struct {
 	} `json:"profile"`
 }
 
+type userResponse struct {
+	User struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Bio      string `json:"bio"`
+		Image    string `json:"image"`
+		Token    string `json:"token"`
+	} `json:"user"`
+}
+
 func newProfileResponse(user *domain.User) *profileResponse {
 	resp := &profileResponse{}
 	resp.Profile.Username = user.Username
 	resp.Profile.Bio = user.Bio
 	resp.Profile.Image = user.Image
 	resp.Profile.Following = user.Following()
+	return resp
+}
+
+func newUserResponse(user *domain.User) *userResponse {
+	resp := &userResponse{}
+	resp.User.Username = user.Username
+	resp.User.Email = user.Email
+	resp.User.Bio = user.Bio
+	resp.User.Image = user.Image
 	return resp
 }
 
