64fb2af785bbf9721d204434b0c3b5caf283caa4
novdov <rpwjd56@gmail.com>
2020-06-21 09:42:57 +0900

    fea: Add userRegisterRequest

---
+++ handler/request.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/novdov/realworld-golang-echo/domain"
+)
+
+type userRegisterRequest struct {
+	User struct {
+		Username string `json:"username" validate:"required"`
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required"`
+	} `json:"user"`
+}
+
+func (u *userRegisterRequest) bind(c echo.Context, user *domain.User) error {
+	if err := c.Bind(&u); err != nil {
+		return err
+	}
+	if err := c.Validate(u); err != nil {
+		return err
+	}
+
+	user.Username = u.User.Username
+	user.Email = u.User.Email
+
+	h, err := user.HashPassword(u.User.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = h
+	return nil
+}
