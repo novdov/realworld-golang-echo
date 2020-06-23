42e3418d87fd5c844f3ab6c7017c5f49aab7763c
novdov <rpwjd56@gmail.com>
2020-06-24 08:30:38 +0900

    fea: Move to user

--- app/main.go
+++ app/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/novdov/realworld-golang-echo/handler"
 	"github.com/novdov/realworld-golang-echo/router"
-	"github.com/novdov/realworld-golang-echo/service"
 	"github.com/novdov/realworld-golang-echo/user"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -39,7 +38,7 @@ func main() {
 	defer client.Disconnect(context.TODO())
 
 	userRepo := user.NewUserRepository(client.Database("real-world"), "user")
-	userService := service.NewUserService(userRepo)
+	userService := user.NewUserService(userRepo)
 
 	r := router.NewRouter()
 	g := r.Group("/api")
--- user/service.go
+++ user/service.go
@@ -1,4 +1,4 @@
-package service
+package user
 
 import (
 	"github.com/novdov/realworld-golang-echo/domain"
