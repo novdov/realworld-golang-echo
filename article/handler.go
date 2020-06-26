f8e907f4bd7286b8b4d918e4499cc2448b60953a
novdov <rpwjd56@gmail.com>
2020-06-26 16:25:05 +0900

    fea: Implement Create

--- article/handler.go
+++ article/handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/novdov/realworld-golang-echo/domain"
 	"github.com/novdov/realworld-golang-echo/errors"
 	"github.com/novdov/realworld-golang-echo/utils"
@@ -14,21 +15,36 @@ type Handler struct {
 	userService    domain.UserService
 }
 
-func NewHandler(as domain.ArticleService) *Handler {
-	return &Handler{articleService: as}
+func NewHandler(as domain.ArticleService, us domain.UserService) *Handler {
+	return &Handler{articleService: as, userService: us}
+}
+
+func (h *Handler) Register(g *echo.Group) {
+	jwtMiddleware := middleware.JWT(utils.JWTSecret)
+
+	article := g.Group("/articles", jwtMiddleware)
+	article.POST("", h.Create)
 }
 
 func (h *Handler) Create(c echo.Context) error {
-	article := domain.Article{
-		Author: utils.GetUserIDFromJWT(c),
+	user, err := h.userService.GetByID(utils.GetUserIDFromJWT(c))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, errors.NewError(err))
+	}
+	if user == nil {
+		return c.JSON(http.StatusNotFound, errors.NewError(errors.NotFound))
+	}
+
+	article := &domain.Article{
+		Author: user.ID,
 	}
 	req := &articleCreateRequest{}
 
-	if err := req.bind(c, &article); err != nil {
+	if err := req.bind(c, article); err != nil {
 		return c.JSON(http.StatusUnprocessableEntity, errors.NewError(err))
 	}
-	if err := h.articleService.Save(&article); err != nil {
+	if err := h.articleService.Save(article); err != nil {
 		return c.JSON(http.StatusUnprocessableEntity, errors.NewError(err))
 	}
-	return c.JSON(http.StatusCreated, new)
+	return c.JSON(http.StatusCreated, newSingleArticleResponse(article, user))
 }
