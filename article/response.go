55aca2b7505729302d6fd3fe6dbf029d251c7b22
SunWoong KIM <35526032+novdov@users.noreply.github.com>
2020-06-29 09:03:11 +0900

    [Comments] Add comments (#11)
    * fea: Add comments
    
    * fea: Fix comment respone fields

--- article/response.go
+++ article/response.go
@@ -41,3 +41,31 @@ func newSingleArticleResponse(article *domain.Article, user *domain.User) *singl
 	resp.Article.Author.Following = user.Following()
 	return resp
 }
+
+type singleCommentResponse struct {
+	Comment struct {
+		ID        string    `json:"id"`
+		Body      string    `json:"body"`
+		CreatedAt time.Time `json:"createdAt"`
+		UpdatedAt time.Time `json:"updatedAt"`
+		Author    struct {
+			Username  string `json:"username"`
+			Bio       string `json:"bio"`
+			Image     string `json:"image"`
+			Following bool   `json:"following"`
+		} `json:"author"`
+	} `json:"comment"`
+}
+
+func newSingleCommentResponse(comment *domain.Comment, user *domain.User) *singleCommentResponse {
+	resp := &singleCommentResponse{}
+	resp.Comment.ID = comment.ID.Hex()
+	resp.Comment.Body = comment.Body
+	resp.Comment.CreatedAt = comment.CreatedAt
+	resp.Comment.UpdatedAt = comment.UpdatedAt
+	resp.Comment.Author.Username = user.Username
+	resp.Comment.Author.Bio = user.Bio
+	resp.Comment.Author.Image = user.Image
+	resp.Comment.Author.Following = user.Following()
+	return resp
+}
